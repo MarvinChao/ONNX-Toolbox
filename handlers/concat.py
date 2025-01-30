976f0fe664a81047326819ef666d9e3bda72367c
Marvin Chao <Marvin@quadric.io>
2025-01-30 10:10:47 -0800

    Adding Concat, Add, and Transpose handlers

---
+++ handlers/concat.py
@@ -0,0 +1,28 @@
+from node_registry import register_node_handler
+from node_attributes import NodeAttributes
+import numpy as np
+
+
+@register_node_handler("Concat")
+class ConcatNodeHandler:
+    def handle(self, model, node):
+        """
+        Handler for op_types "Concat".
+
+        * This is a pure memory transfer op, there is no compute with it
+
+        Args:
+            model (class):  Input ONNX model
+            node (class):   ONNX node
+
+        Returns:
+            attributes (class): Node attributes
+        """
+        attributes = NodeAttributes(model, node)
+
+        # Add inputs could possibly contains coefficients
+        for tensor_name in node.input:
+            if attributes.is_tensor_name_initializer(model, tensor_name):
+                attributes.weight_size += attributes.get_weight_size(model, tensor_name)
+
+        return attributes
